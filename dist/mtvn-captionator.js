aaad63ec5b5edb9999db9524f71af183ffaff3a9
giannif <giannif@gmail.com>
2014-01-16 16:56:42 -0500

    Process captions all at once for VTT.

--- dist/mtvn-captionator.js
+++ dist/mtvn-captionator.js
@@ -4,7 +4,7 @@
 	Share and enjoy
 	https://github.com/cgiffard/Captionator
 
-	built: 01/16/2014 02:37:18 PM
+	built: 01/16/2014 04:56:38 PM
 */
 /* jshint strict:true */
 (function() {
@@ -689,6 +689,9 @@
 		}
 	};
 	captionator.ttmlToVtt = function(ttml) {
+		if (!ttml) {
+			return "";
+		}
 		return ttml.replace("<br>", "\n").replace("</br>", "");
 	};
 	/* global TextTrackCue */
@@ -697,63 +700,23 @@
 	 * First parameter: HTMLVideoElement object with associated TextTracks
 	 */
 	captionator.rebuildCaptionsVTT = function(videoElement) {
-		var trackList = videoElement._textTracks || [],
-			currentTime = videoElement.currentTime,
-			compositeActiveCues = [],
-			cuesChanged = false,
-			activeCueIDs = [],
-			cueSortArray = [];
-		if (!videoElement._track) {
-			videoElement._track = videoElement.addTextTrack("captions");
-		}
-		var track = videoElement._track;
-		track.mode = "showing";
-		// Work out what cues are showing...
+		var trackList = videoElement._textTracks || [];
 		trackList.forEach(function(track) {
+			if (track.vttProcessed) {
+				return;
+			}
 			if (track.mode === captionator.TextTrack.SHOWING && track.readyState === captionator.TextTrack.LOADED) {
-				cueSortArray = [].slice.call(track.activeCues, 0);
-				// Do a reverse sort
-				// Since the available cue render area is a square which decreases in size
-				// (away from each side of the video) with each successive cue added,
-				// and we want cues which are older to be displayed above cues which are newer,
-				// we sort active cues within each track so that older ones are rendered first.
-	
-				cueSortArray = cueSortArray.sort(function(cueA, cueB) {
-					if (cueA.startTime > cueB.startTime) {
-						return -1;
-					} else {
-						return 1;
+				track.vttProcessed = true;
+				var newTrack = videoElement.addTextTrack("captions");
+				newTrack.mode = "showing";
+				track.cues.forEach(function(cue) {
+					if (cue.track.kind !== "metadata" && cue.mode !== captionator.TextTrack.HIDDEN) {
+						var processed = captionator.ttmlToVtt(cue.text.toString());
+						newTrack.addCue(new TextTrackCue(cue.startTime, cue.endTime, processed));
 					}
 				});
-	
-				compositeActiveCues = compositeActiveCues.concat(cueSortArray);
 			}
 		});
-	
-		// Determine whether cues have changed - we generate an ID based on track ID, cue ID, and text length
-		activeCueIDs = compositeActiveCues.map(function(cue) {
-			return cue.track.id + "." + cue.id + ":" + cue.text.toString(currentTime).length;
-		});
-		cuesChanged = !captionator.compareArray(activeCueIDs, videoElement._captionator_previousActiveCues);
-		// If they've changed, we re-render our cue canvas.
-		if (cuesChanged || videoElement._captionator_dirtyBit) {
-			// If dirty bit was set, it certainly isn't now.
-			videoElement._captionator_dirtyBit = false;
-	
-			// Destroy internal tracking variable (which is used for caption rendering)
-			videoElement._captionator_availableCueArea = null;
-	
-			// Internal tracking variable to determine whether our composite active cue list for the video has changed
-			videoElement._captionator_previousActiveCues = activeCueIDs;
-	
-			// Now we render the cues
-			compositeActiveCues.forEach(function(cue) {
-				if (cue.track.kind !== "metadata" && cue.mode !== captionator.TextTrack.HIDDEN) {
-					var processed = captionator.ttmlToVtt(cue.text.processedCue);
-					track.addCue(new TextTrackCue(cue.startTime, cue.endTime, processed));
-				}
-			});
-		}
 	};
 	captionator.mtvnProcessOutput = function(text) {
 		if (!text) {
